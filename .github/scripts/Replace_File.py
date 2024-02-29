4cf146c9077f67adf207834589e3f9cd6278129b
Hao-wei Wu <60650989+User-Howard@users.noreply.github.com>
2024-02-29 22:33:06 +0800

    Update Replace_File.py

--- .github/scripts/Replace_File.py
+++ .github/scripts/Replace_File.py
@@ -5,28 +5,22 @@ def move_files(source_dir):
     """
     将指定目录下的特定文件移动到另一个指定目录
     """
-    # 获取源目录下的所有文件名
+    
     filenames = os.listdir(source_dir)
     print('filenames', filenames)
     for filename in filenames:
-        # 判断是否是目标文件
         if "readme" not in filename.lower():
-            # 构造源文件路径和目标文件路径
             
-            mp = {"ZEROJUDGE":"ZeroJudge", "CODEFORCES":"CodeForces", "TIOJ":"TIOJ", "APCS":"APCS", "NEOJ":"NEOJ", "POJ":"POJ", "VJUDGE":"VJudge", "CSES":"CSES"}
-            # 移动文件
-            
-            
-            judge = None
-            for name in mp:
-              if name in filename:
-                judge = name
-            if judge == None:
+            if filename.find('-') != -1:
                 continue
+
+            
+            judge = filename.split('-')[0]
+            
             src_path = os.path.join(source_dir, filename)
             
             problem = filename.replace(f"{judge}-", "")
-            problem_store_folder = f"./Solutions/{mp[judge]}/{os.path.splitext(problem)[0]}"
+            problem_store_folder = f"./Solutions/{judge.upper()}/{os.path.splitext(problem)[0]}"
 
             if not os.path.exists(problem_store_folder):
                os.makedirs(problem_store_folder)
@@ -36,6 +30,5 @@ def move_files(source_dir):
                 
 
 if __name__ == "__main__":
-    # 设置源目录和目标目录
     source_dir = "."
     move_files(source_dir)
