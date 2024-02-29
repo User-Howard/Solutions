d138feec410752b08dfaac3c4e476be11a2046ab
Hao-wei Wu <60650989+User-Howard@users.noreply.github.com>
2024-02-29 23:15:08 +0800

    Update Replace_File.py

--- .github/scripts/Replace_File.py
+++ .github/scripts/Replace_File.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import datetime
 
 def move_files(source_dir):
     """
@@ -23,7 +24,45 @@ def move_files(source_dir):
         dst_path = os.path.join(problem_store_folder, os.path.splitext(problem)[0]+f"-{len(os.listdir(problem_store_folder))}"+os.path.splitext(problem)[1])
         shutil.move(src_path, dst_path)
                 
+                
+def update_readme(source_dir):
+    Solutions = {}
+    cnt_all = 0
+    cnt = 0
+    for folder in sorted(os.listdir(source_dir)):
+        Solutions[folder] = []
+        folder_path = os.path.join(source_dir, folder)
+        for question in sorted(os.listdir(folder_path)):
+            question_path = os.path.join(folder_path, question)
+            Solutions[folder].append((question, f"https://github.com/User-Howard/Solutions/tree/main/Solutions/{folder}/{question}"))
 
+            cnt_all += len(os.listdir(question_path))
+            cnt += 1
+
+    content = """
+# Solutions
+
+要互助這一個資料庫才可以好，不然現在看起來弱弱的。
+
+上傳在子目錄即可
+檔案名稱如下
+```
+平台名稱-題目編號.副檔名
+```
+
+如
+```ZEROJUDGE-a001.cpp```
+
+"""
+    for i in Solutions:
+        content += f"### {i}\n"
+        for j in Solutions[i]:
+            content += f"[`{j[0]}`]({j[1]}) "
+        content += "\n"
+
+    content += f'Last Updated: {datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}'
+    with open("README.md", 'w') as file:
+        file.write(content)
 if __name__ == "__main__":
-    source_dir = "."
-    move_files(source_dir)
+    move_files(".")
+    update_readme("./Solutions")
